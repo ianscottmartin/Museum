1a6687b740e9516fcf1661caa833f10712e9e6f5
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-21 16:29:07 -0600

    add treasure

--- script.js
+++ script.js
@@ -34,6 +34,50 @@ const renderCards = (dataArray) => {
         cardContainer.appendChild(imgContainer);
     });
 };
+const fetchDataCat = () => {
+    return fetch("https://botw-compendium.herokuapp.com/api/v2")
+        .then(response => response.json())
+        .then(info => {
+            // console.log(info.data)
+            renderLinksCards(info.data.monsters)
+            return info
+        })
+}
+
+const renderLinksCards = (dataArray) => {
+    const cardContainer = document.getElementById("card-container");
+    dataArray.forEach((cardInfo) => {
+        const imgContainer = document.createElement("div");
+
+        const img = document.createElement("img");
+        const name = document.createElement("p")
+        // const description = document.createElement("p")
+        // const equipment = document.createElement('p')
+
+
+        name.textContent = cardInfo.name
+        // description.textContent = cardInfo.description
+        // equipment.textContent = cardInfo.equipment
+
+        imgContainer.append(img, name)
+
+        img.src = cardInfo.image;
+
+        img.addEventListener("dblclick", (event) => {
+            // console.log(cardInfo)
+            const description = document.createElement("p")
+            description.textContent = cardInfo.description
+            const pName = document.createElement("p")
+            pName.textContent = cardInfo.name
+            // console.log(pName)
+            imgContainer.append(img, description)
+        })
+
+
+
+        cardContainer.appendChild(imgContainer);
+    });
+};
 
 
 const fetchData = () => {
@@ -41,7 +85,7 @@ const fetchData = () => {
         .then(response => response.json())
         .then(info => {
             // console.log(info.data)
-            renderCards(info.data.monsters)
+            renderCards(info.data.treasure)
             return info
         })
 }
@@ -55,10 +99,10 @@ const addEventListenerToNavLinks = () => {
             console.log(e.target)
             // debugger
             // a. Return only images that match the clicked link
-            fetch("https://botw-compendium.herokuapp.com/api/v2/category/<category>")
+            fetch("https://botw-compendium.herokuapp.com/api/v2")
                 .then(response => response.json())
                 .then(info => {
-
+                    console.log(e.target)
                     // renderCards(info.data.monsters)
                     // renderCards(info.data.creatures)
                     // renderCards(info.data.equipment)
@@ -80,6 +124,7 @@ const addEventListenerToNavLinks = () => {
 
 addEventListenerToNavLinks()
 fetchData()
+fetchDataCat()
 
 // const renderCards = (dataArray) => {
 //     const cardContainer = document.getElementById("card-container");
